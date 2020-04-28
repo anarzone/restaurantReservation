0cc87b0d8d6259e9c5a91709799370fde7defc44
anarzone <haqverdiyevanar@gmail.com>
2020-04-28 17:17:26 +0400

    removed breadcrumb

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -30,8 +30,8 @@
                                 <div class="d-flex align-items-center">
                                     <nav aria-label="breadcrumb">
                                         <ol class="breadcrumb m-0 p-0">
-                                            <li class="breadcrumb-item"><a href="index.html">Dashboard</a>
-                                            </li>
+{{--                                            <li class="breadcrumb-item"><a href="index.html">Dashboard</a>--}}
+{{--                                            </li>--}}
                                         </ol>
                                     </nav>
                                 </div>
