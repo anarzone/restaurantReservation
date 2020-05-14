65e6b0ad409bfb865c615781230b0355650416e1
anarzone <haqverdiyevanar@gmail.com>
2020-05-14 15:46:27 +0400

    merged login

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -1,17 +1,14 @@
-
 <!DOCTYPE html>
 <html dir="ltr">
-
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
     <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicon.png">
-    <title>Adminmart Template - The Ultimate Multipurpose admin template</title>
+    <title>Amburan Reservation</title>
     <!-- Custom CSS -->
     <link href="{{asset('back/dist/css/style.min.css')}}" rel="stylesheet">
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
@@ -24,27 +21,14 @@
 
 <body>
 <div class="main-wrapper">
-    <!-- ============================================================== -->
-    <!-- Preloader - style you can find in spinners.css -->
-    <!-- ============================================================== -->
-    <div class="preloader">
-        <div class="lds-ripple">
-            <div class="lds-pos"></div>
-            <div class="lds-pos"></div>
-        </div>
-    </div>
-    <!-- ============================================================== -->
-    <!-- Preloader - style you can find in spinners.css -->
-    <!-- ============================================================== -->
-    <!-- ============================================================== -->
-    <!-- Login box.scss -->
-    <!-- ============================================================== -->
-    <div class="auth-wrapper d-flex no-block justify-content-center align-items-center position-relative"
-         style="background:url(assets/images/big/auth-bg.jpg) no-repeat center center;">
-        <div class="auth-box row justify-content-center align-items-center">
-            <div class="col-lg-8 col-md-7 bg-white">
+
+    <div class="auth-wrapper d-flex no-block justify-content-center align-items-center position-relative">
+        <div class="row justify-content-center align-items-center">
+            <div class="col-lg-8 col-md-7 bg-white bg-auth">
                 <div class="p-3">
-                    <h2 class="mt-3 text-center">Giriş</h2>
+                    <div class="text-center">
+                        <img width="150" src="{{asset('back/assets/images/logo.png')}}" alt="">
+                    </div>
                     <form class="mt-4" method="POST" action="{{ route('login') }}">
                         @csrf
                         <div class="row">
@@ -71,18 +55,12 @@
             </div>
         </div>
     </div>
-    <!-- ============================================================== -->
-    <!-- Login box.scss -->
-    <!-- ============================================================== -->
 </div>
 
 <script src="{{asset('back/assets/libs/jquery/dist/jquery.min.js')}}"></script>
 <script src="{{asset('back/assets/libs/popper.js/dist/umd/popper.min.js')}}"></script>
 <script src="{{asset('back/assets/libs/bootstrap/dist/js/bootstrap.min.js')}}"></script>
 
-<script>
-    $(".preloader ").fadeOut();
-</script>
 </body>
 
 </html>
