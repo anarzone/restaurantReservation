a5dde231b9f42b55f907346b7398f6c11a3fc739
anarzone <haqverdiyevanar@gmail.com>
2020-06-01 11:04:43 +0400

    select latest customers

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -15,11 +15,6 @@ class CustomerController extends Controller
            'customers' => Customer::latest()->paginate(10),
         ]);
     }
-//Select r.*, r2.name, h.name FROM reservations r
-//LEFT JOIN customers c on r.customer_id = c.id
-//LEFT JOIN restaurants r2 on r.res_restaurant_id = r2.id
-//LEFT JOIN halls h on r2.id = h.restaurant_id
-//WHERE customer_id=1
     public function getReservationsByCustomer(Customer $customer){
         $reservation_info = Reservation::leftJoin('customers  as c', 'reservations.customer_id', '=', 'c.id')
                                        ->leftjoin('restaurants as r', 'reservations.res_restaurant_id', '=', 'r.id')
