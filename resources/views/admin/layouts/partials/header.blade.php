d4c558d0c2c927610a0bff3b70a6469c1ec99f4b
anarzone <haqverdiyevanar@gmail.com>
2020-06-18 17:39:05 +0400

    prevented to be responsible in other devices

--- resources/views/admin/layouts/partials/header.blade.php
+++ resources/views/admin/layouts/partials/header.blade.php
@@ -10,8 +10,6 @@
     <title>Amburan Admin</title>
     <!-- Custom CSS -->
     <link href="{{asset('back/assets/extra-libs/c3/c3.min.css')}}" rel="stylesheet">
-    <link href="{{asset('back/assets/libs/chartist/dist/chartist.min.css')}}" rel="stylesheet">
-    <link href="{{asset('back/assets/extra-libs/jvector/jquery-jvectormap-2.0.2.css')}}" rel="stylesheet" />
     <!-- Custom CSS -->
     <link href="{{asset('back/dist/css/style.min.css')}}?v=1.2" rel="stylesheet">
     <link rel="stylesheet" href="https:////cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
