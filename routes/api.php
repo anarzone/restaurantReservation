6897314a75fc99e7bdd4743d6d447b06fcae39c0
Suleyman Aliyev <suleymanali76@gmail.com>
2020-11-06 10:26:31 +0400

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,6 @@ use Illuminate\Http\Request;
 
 Route::prefix('v1')->namespace('Api\V1')->group(function (){
     Route::get('getRestaurants', 'FormController@getRestaurants')->name('getRestaurants');
-    Route::get('getHallsByRestaurantId/{restaurant}', 'FormController@getRestaurants')->name('getRestaurants');
+    Route::get('getHallsByRestaurantId/{restaurant}', 'FormController@getHallsByRestaurantId')->name('getRestaurants');
 });
 Route::post('/v1/sendForm', 'FrontController@sendForm');
